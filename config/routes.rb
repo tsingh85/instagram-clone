16da4244cee1e2b943735a40cceab5af1cb8e7e6
Tejveer Singh <tejveer.bath@kin.com>
2024-06-30 18:59:11 -0600

    Added devise and cleaned UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get 'users', to: 'users/registrations#new'
+    get 'users/password', to: 'users/passwords#new'
+  end
+  devise_for :users
+  
+  get 'home/about'
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
